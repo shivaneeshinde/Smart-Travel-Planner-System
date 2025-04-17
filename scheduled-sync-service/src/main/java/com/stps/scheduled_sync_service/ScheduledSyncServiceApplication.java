c1057aace915415296eaefc1d03498ff00f017ad
Shivanee Deepak Shinde <shivaneeshinde@gmail.com>
2025-04-17 13:03:36 +0530

    services pushed

---
+++ scheduled-sync-service/src/main/java/com/stps/scheduled_sync_service/ScheduledSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stps.scheduled_sync_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledSyncServiceApplication.class, args);
+	}
+
+}
---
+++ scheduled-sync-service/src/test/java/com/stps/scheduled_sync_service/ScheduledSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stps.scheduled_sync_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledSyncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
