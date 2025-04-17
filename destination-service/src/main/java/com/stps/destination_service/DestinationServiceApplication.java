9d4db8d5184448be7265f82d0782862a02944d31
Shivanee Deepak Shinde <shivaneeshinde@gmail.com>
2025-04-17 12:56:50 +0530

    destination service

---
+++ destination-service/src/main/java/com/stps/destination_service/DestinationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stps.destination_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinationServiceApplication.class, args);
+	}
+
+}
---
+++ destination-service/src/test/java/com/stps/destination_service/DestinationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stps.destination_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
