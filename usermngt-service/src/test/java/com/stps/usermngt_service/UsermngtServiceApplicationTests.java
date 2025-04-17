69a63e30731454955ea12a8055393527e58e8df7
Shivanee Deepak Shinde <shivaneeshinde@gmail.com>
2025-04-17 12:05:25 +0530

    User management service
    User management service

---
+++ usermngt-service/src/test/java/com/stps/usermngt_service/UsermngtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stps.usermngt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermngtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
